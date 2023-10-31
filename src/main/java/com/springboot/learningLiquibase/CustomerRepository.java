a249960ffc6ba8f2f948b67d4f93d85a23d4155e
Ajay Singh <asajay.cs@gmail.com>
2023-10-31 17:27:17 +0530

    Initial commit

---
+++ src/main/java/com/springboot/learningLiquibase/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.springboot.learningLiquibase;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository <Customer ,Integer> {
+}
